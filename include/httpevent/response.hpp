122ed0d51b1d0d5719500073273557e703adaea1
centos7 <centos7@localhost>
2017-04-17 23:28:27 +0800

    解决empty body响应问题

--- include/httpevent/response.hpp
+++ include/httpevent/response.hpp
@@ -17,14 +17,16 @@ namespace httpevent {
         : req(req)
         , headers(evhttp_request_get_output_headers(req))
         , res(evhttp_request_get_output_buffer(req))
-        , sent(false) {
+        , sent(false)
+        , temp_body() {
         }
 
         response(const response& other) :
         req(other.req)
         , headers(other.headers)
         , res(other.res)
-        , sent(other.sent) {
+        , sent(other.sent)
+        , temp_body(other.temp_body) {
         }
 
         response& operator=(const response& right) {
@@ -35,6 +37,7 @@ namespace httpevent {
                 this->headers = right.headers;
                 this->res = right.res;
                 this->sent = right.sent;
+                this->temp_body = right.temp_body;
             }
 
             return *this;
@@ -79,19 +82,19 @@ namespace httpevent {
             }
         }
 
-        std::string get_response_data() {
-            std::string result;
+        const std::string& get_response_data() {
             if (!sent) {
-                std::size_t buf_size = evbuffer_get_length(res);
+                std::size_t buf_size = evbuffer_get_length(this->res);
                 if (buf_size) {
                     char buf_data[buf_size + 1];
-                    ev_ssize_t n = evbuffer_remove(res, buf_data, buf_size + 1);
-                    if (n >= 0) {
-                        result.assign(buf_data, n);
+                    ev_ssize_t n = evbuffer_remove(this->res, buf_data, buf_size + 1);
+                    if (n > 0) {
+                        this->temp_body.assign(buf_data, n);
                     }
                 }
             }
-            return result;
+            this->send_body(this->temp_body);
+            return this->temp_body;
         }
 
         bool is_sent()const {
@@ -103,6 +106,7 @@ namespace httpevent {
         struct evkeyvalq *headers;
         struct evbuffer *res;
         bool sent;
+        std::string temp_body;
     };
 }
 
